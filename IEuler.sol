47c83fb83ea7eec5e4456b986075b0e706b36fcf
Doug Hoyte <doug@hcsw.org>
2021-11-16 00:10:49 -0500

    update interface

--- IEuler.sol
+++ IEuler.sol
@@ -1,11 +1,16 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0;
 pragma abicoder v2;
 
 
 /// @notice Main storage contract for the Euler system
 interface IEuler {
-    /// @notice Lookup a proxy that can be used to interact with a module
+    /// @notice Lookup the current implementation contract for a module
+    /// @param moduleId Fixed constant that refers to a module type (ie MODULEID__ETOKEN)
+    /// @return An internal address specifies the module's implementation code
+    function moduleIdToImplementation(uint moduleId) external view returns (address);
+
+    /// @notice Lookup a proxy that can be used to interact with a module (only valid for single-proxy modules)
     /// @param moduleId Fixed constant that refers to a module type (ie MODULEID__MARKETS)
     /// @return An address that should be cast to the appropriate module interface, ie IEulerMarkets(moduleIdToProxy(2))
     function moduleIdToProxy(uint moduleId) external view returns (address);
